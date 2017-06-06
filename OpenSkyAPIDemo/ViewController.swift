0fd24421c7d271fb0992c0fdcdda8b5a837cc82b
ginpei <ginpei@gmail.com>
2017-06-06 16:06:10 -0700

    fetch data via the API

--- OpenSkyAPIDemo/ViewController.swift
+++ OpenSkyAPIDemo/ViewController.swift
@@ -14,6 +14,48 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func fetch_touchUpInside(_ sender: UIButton) {
+        print("loading...")
+        
+        var request = URLRequest(url: URL(string: "https://opensky-network.org/api/states/all")!)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) {data, response, err in
+            if err != nil {
+                print("ERR")
+            }
+            else {
+                do {
+                    try self.pickUpData(from: data)
+                } catch let error as NSError {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
+    
+    func pickUpData(from data: Data?) throws -> Any {
+        let json = try JSONSerialization.jsonObject(with: data!, options:.allowFragments) as! [String : AnyObject]
+        for (key, value) in json {
+            print(key)
+        }
+        if let stateList = json["states"] as? [[Any]] {
+            for row in stateList {
+                if
+                    let icao24 = row[0] as? String,
+                    let originCountry = row[2] as? String,
+                    let longitude = row[5] as? Double
+                {
+                    print("\(icao24), \(originCountry), \(longitude)")
+                }
+            }
+        }
+        else {
+            print("BOOOOOO")
+        }
+        return json
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
